fb92a23bb148c4cee94befe73724a8837d36cd6c
小丑路人 <2278757482@qq.com>
2020-12-17 00:08:17 +0800

    文件配置与上传文件

---
+++ app/Modules/Admin/Http/Controllers/UploadController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Modules\Admin\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class UploadController extends BaseController
+{
+    /**
+     * 文件上传
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string                    $file
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function file(Request $request, $file = 'file')
+    {
+        if (empty($request->file($file))){
+            return $this->errorJson('请上传文件！');
+        }
+
+        $path = $request->file($file)->storePublicly(
+            date('Ym'),
+            config('filesystems')
+        );
+
+        return $this->successJson($path, '上传成功', ['path_url' => Storage::url($path)]);
+    }
+}
