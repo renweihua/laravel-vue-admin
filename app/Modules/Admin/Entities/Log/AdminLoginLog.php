8286d54eaba02754e8b684fbfac0ffe9ff3b76e5
小丑路人 <2278757482@qq.com>
2020-12-18 19:00:35 +0800

    管理员日志关联管理员模型

--- app/Modules/Admin/Entities/Log/AdminLoginLog.php
+++ app/Modules/Admin/Entities/Log/AdminLoginLog.php
@@ -3,9 +3,15 @@
 namespace App\Modules\Admin\Entities\Log;
 
 use App\Models\MonthModel;
+use App\Modules\Admin\Entities\Rabc\Admin;
 
 class AdminLoginLog extends MonthModel
 {
     protected $primaryKey = 'log_id';
     protected $is_delete = 0;
+
+    public function admin()
+    {
+        return $this->belongsTo(Admin::class, 'admin_id', 'admin_id');
+    }
 }
--- app/Modules/Admin/Entities/Rabc/AdminMenu.php
+++ app/Modules/Admin/Entities/Rabc/AdminMenu.php
@@ -8,9 +8,14 @@ class AdminMenu extends Model
 {
     protected $primaryKey = 'menu_id';
     protected $is_delete = 0;
-    
+
     public static function getAllMenus()
     {
         return self::orderBy('menu_sort', 'ASC')->get();
+    }
+
+    public function getSelectLists()
+    {
+        return list_to_tree($this->orderBy('menu_sort', 'ASC')->get()->toArray());
     }
 }
