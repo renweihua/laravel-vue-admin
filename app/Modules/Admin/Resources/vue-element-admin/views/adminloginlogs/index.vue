dae30f6023f31de16c27a953999bff50bfac2482
小丑路人 <2278757482@qq.com>
2021-01-07 22:37:37 +0800

    同步使用多语言包。

--- app/Modules/Admin/Resources/vue-element-admin/views/adminloginlogs/index.vue
+++ app/Modules/Admin/Resources/vue-element-admin/views/adminloginlogs/index.vue
@@ -32,7 +32,7 @@
                 {{ $t('table.search') }}
             </el-button>
             <el-button v-waves class="filter-item" type="danger" icon="el-icon-delete" @click="handleDelete">
-                批量删除
+                {{ $t('table.batchDelete') }}
             </el-button>
         </div>
 
