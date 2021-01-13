b6cf2f5259cc7259c8dbf93a872baf9c8342ea45
小丑路人 <2278757482@qq.com>
2021-01-13 18:11:00 +0800

    input的排序全部 设置类型：number。

--- app/Modules/Admin/Resources/vue-element-admin/views/friendlinks/components/detail.vue
+++ app/Modules/Admin/Resources/vue-element-admin/views/friendlinks/components/detail.vue
@@ -40,7 +40,7 @@
                 <el-input v-model.trim="form.link_url" autocomplete="off"/>
             </el-form-item>
             <el-form-item label="排序" prop="link_sort">
-                <el-input v-model.trim="form.link_sort" autocomplete="off"/>
+                <el-input v-model.trim="form.link_sort" type="number" autocomplete="off"/>
             </el-form-item>
             <el-form-item label="是否启用" prop="is_check">
                 <el-radio-group v-model="form.is_check">
